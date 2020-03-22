b55e1a89751b71405fb0f5ed21662c6511f69bc8
Olga <norinaoa@mail.ru>
2020-03-23 00:06:09 +0500

    Constructor added

--- src/test/java/ru/netology/domain/RadiofanTest.java
+++ src/test/java/ru/netology/domain/RadiofanTest.java
@@ -10,20 +10,15 @@ class RadiofanTest {
     @ParameterizedTest
     @CsvFileSource(resources = "/dataForNextRS.csv")
     void nextRadioStation(boolean on, int currentRadioStation, int expected) {
-        Radiofan radiofan = new Radiofan();
-        radiofan.setOn(on);
-        radiofan.setCurrentRadioStation(currentRadioStation);
+        Radiofan radiofan = new Radiofan("nextRS",0,currentRadioStation,0,0,on);
         radiofan.nextRadioStation();
-
         assertEquals(expected, radiofan.getCurrentRadioStation());
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/dataForPrevRS.csv")
     void prevRadioStation(boolean on, int currentRadioStation, int expected) {
-        Radiofan radiofan = new Radiofan();
-        radiofan.setOn(on);
-        radiofan.setCurrentRadioStation(currentRadioStation);
+        Radiofan radiofan = new Radiofan("prevRS",0, currentRadioStation,0,0,on);
         radiofan.prevRadioStation();
 
         assertEquals(expected, radiofan.getCurrentRadioStation());
@@ -32,8 +27,7 @@ class RadiofanTest {
     @ParameterizedTest
     @CsvFileSource(resources = "/dataForInputRS.csv")
     void inputRadioStation(boolean on, int currentRadioStation, int expected) {
-        Radiofan radiofan = new Radiofan();
-        radiofan.setOn(on);
+        Radiofan radiofan = new Radiofan("inputRS",0,0,0,0,on);
         radiofan.setCurrentRadioStation(currentRadioStation);
 
         assertEquals(expected, radiofan.getCurrentRadioStation());
@@ -42,9 +36,7 @@ class RadiofanTest {
     @ParameterizedTest
     @CsvFileSource(resources = "/dataForIncSV.csv")
     void increaseSoundVolume(boolean on, int currentSoundVolume, int expected) {
-        Radiofan radiofan = new Radiofan();
-        radiofan.setOn(on);
-        radiofan.setCurrentSoundVolume(currentSoundVolume);
+        Radiofan radiofan = new Radiofan("incSV",0,0,0,currentSoundVolume,on);
         radiofan.increaseSoundVolume();
 
         assertEquals(expected, radiofan.getCurrentSoundVolume());
@@ -53,9 +45,7 @@ class RadiofanTest {
     @ParameterizedTest
     @CsvFileSource(resources = "/dataForDecSV.csv")
     void decreaseSoundVolume(boolean on, int currentSoundVolume, int expected) {
-        Radiofan radiofan = new Radiofan();
-        radiofan.setOn(on);
-        radiofan.setCurrentSoundVolume(currentSoundVolume);
+        Radiofan radiofan = new Radiofan("decSV",0,0,-10,currentSoundVolume,on);
         radiofan.decreaseSoundVolume();
 
         assertEquals(expected, radiofan.getCurrentSoundVolume());
